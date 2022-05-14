c01de18f9b3e33f9ce1c32a05476224b4499d9e7
Rokyuto <93655294+Rokyuto@users.noreply.github.com>
2022-05-14 14:37:05 +0300

    Added Bot Commands. Not Completed

--- Bot/KPoPTracker.py
+++ Bot/KPoPTracker.py
@@ -23,15 +23,6 @@ async def CheckImage(bot):
     newestElement = newestCategory.find(class_='cell') # Get the Newest Element in the Category
     
     newestElementLink = newestElement.find('a')['href'] # Get the Newest Element' Image
-    
-    
-    
-    
-    #IMPLEMENT THE BRANCH FOR FILTERING
-    
-    
-    
-    
 
     #newestElementName = newestElementImg['src'] # Get the Newest Element Name
     #newestElementNameFormated = newestElementName.replace(" ", "-").replace("---", "-") # Forma the Newest Element Name
