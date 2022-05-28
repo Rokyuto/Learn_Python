c177d9db618af0a0fcb940ae9c01efa2499416cb
Rokyuto <93655294+Rokyuto@users.noreply.github.com>
2022-05-28 13:29:11 +0300

    Added Tracking All KPOPs when Track List is empty

--- Bot/KPoPTracker.py
+++ Bot/KPoPTracker.py
@@ -5,6 +5,9 @@ from bs4 import BeautifulSoup
 import KPoPPost
 
 # Initialize Variables
+baseURL = 'https://kpopping.com/kpics/gender-female/category-all/idol-any/group-any/order' # Website Default URL
+lastImage = None
+newestImage = None
 newestElementHREF = None # Last Image Initial Value
 lastKPOPImagesList = [] # List with Last Image for each KPOP Link
 
@@ -16,19 +19,14 @@ def f_FillLastKPOPImagesList(KPOPLinksList):
 
 def FindNewestImage(KPOPLink):
     global newestElementHREF
-    #URL = 'https://kpopping.com/kpics/gender-female/category-all/idol-any/group-any/order' # Website URL
     URL = KPOPLink
 
     page = requests.get(URL) # Call the Website
-    
     soup = BeautifulSoup(page._content, 'html.parser') # Parse the Website to be able to get information || Get all information in the website 
-    
     images = soup.find(class_='box pics infinite') # Get Container with all Idols Images
     
     newestCategory = images.find(class_='matrix matrix-breezy mb-2') # Get the Newest Children Idols Images Container
-    
     newestElement = newestCategory.find(class_='cell') # Get the Newest Element in the Category
-    
     newestElementHREF = newestElement.find('a')['href'] # Get the Newest Element' Image
     
     return newestElementHREF # Return and Update the newest Element href 
@@ -38,33 +36,35 @@ async def CheckImage(bot,KPOPLinksList):
     # Check if the KPOP Links List has any Links
     if (len(KPOPLinksList) > 0): # If TRUE then it will go Check for new Images in this Link else (if false) the bot won't do anything
         for link in KPOPLinksList:          
-            #global lastImage # Initialize Last Image
-            FindNewestImage(link) # Call Finding Newest Image href function
+            f_SearchMachine(link) # Call Funtion to get the newest Image Photo from newest Element HREF
             
-            #newestElementName = newestElementImg['src'] # Get the Newest Element Name
-            #newestElementNameFormated = newestElementName.replace(" ", "-").replace("---", "-") # Forma the Newest Element Name
-            
-            newestElementLink = "https://kpopping.com% s" % newestElementHREF #  Newest Element Website URL
-
-            newestElementPage = requests.get(newestElementLink)# Newest Element URL 
-            
-            newestElementSoup = BeautifulSoup(newestElementPage._content, 'html.parser') # Parse the Website to be able to get information || Get all information in the website 
-            
-            newestImageDiv = newestElementSoup.find(class_='justified-gallery') # Get the Newest Element Div Object
-            
-            newestImageLink = newestImageDiv.find('img')['src'] # Get the Newest Element Link / Src
-            
-            newestImageLinkTrim = urlparse(newestImageLink).path # Trim the Image to Remove Everithing after Image Format (.jpeg)
-
-            newestImage = "https://kpopping.com% s" % newestImageLinkTrim # Build newest Image ImageURL
-
             currentLinkIndex = KPOPLinksList.index(link) # Get the Index of the current Link
-            
-            # if newestImage != lastImage: # If the newest image is not the same as the last image
-            #     await KPoPPost.ImagePost(bot, newestImage) # Call KPoPPost.ImagePost() Function
-            #     lastImage = newestImage # Update last Image
             
             if newestImage != lastKPOPImagesList[currentLinkIndex]:
                 await KPoPPost.ImagePost(bot, newestImage) # Call KPoPPost.ImagePost() Function
                 lastKPOPImagesList[currentLinkIndex] = newestImage # Update the last Image for current KPoP
                 
+    else:
+        global lastImage # Initialize Last Image
+        f_SearchMachine(baseURL)
+        
+        if newestImage != lastImage: # If the newest image is not the same as the last image
+                await KPoPPost.ImagePost(bot, newestImage) # Call KPoPPost.ImagePost() Function
+                lastImage = newestImage # Update last Image
+
+
+# Funtion to get the newest Image from newest Element HREF
+def f_SearchMachine(link):
+    FindNewestImage(link) # Call Finding Newest Image href function
+    global newestImage
+    
+    newestElementLink = "https://kpopping.com% s" % newestElementHREF #  Newest Element Website URL
+    newestElementPage = requests.get(newestElementLink)# Newest Element URL 
+    newestElementSoup = BeautifulSoup(newestElementPage._content, 'html.parser') # Parse the Website to be able to get information || Get all information in the website 
+    
+    newestImageDiv = newestElementSoup.find(class_='justified-gallery') # Get the Newest Element Div Object
+    newestImageLink = newestImageDiv.find('img')['src'] # Get the Newest Element Link / Src  
+    newestImageLinkTrim = urlparse(newestImageLink).path # Trim the Image to Remove Everithing after Image Format (.jpeg)
+    newestImage = "https://kpopping.com% s" % newestImageLinkTrim # Build newest Image ImageURL
+    
+    return newestImage
